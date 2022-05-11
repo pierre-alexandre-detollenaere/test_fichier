c084dcd0a5cbf8679b0892d960ea4fa981eaef70
pierre-alexandre <pa.detollenaere@gmail.com>
2022-05-11 15:37:13 +0200

    ajout de commentaires et créations de variables age et nom

--- test_javascript.js
+++ test_javascript.js
@@ -1,1 +1,29 @@
 alert("Vous êtes bien sur notre site !");
+
+// on a appris à modifier une div : //
+document.getElementById("madiv").innerHTML="le nouveau text";
+//document.getElementById("madiv") : qui va récupérer l'emplacement de la div
+//.innerHTML="le nouveau text"; qui modifie le HTML de la div
+
+/* ALGORITHMIQUE
+
+1 VARIABLES
+Lorsqu'on va stocker ou manipuler on va avoir besoin de données 
+qu'on va mettre dans des variables
+
+2 CONDITIONS
+
+3 BOUCLES
+
+4 FONCTIONS
+
+*/
+// VARIABLES NUMERIQUES
+//CREATION DE LA VARIABLE NUMERIQUE NOM CONTENANT 34
+age=34;
+alert("age");
+
+// VARIABLES CHAINES DE CARATERES
+//CREATION DE LA CHAINE DE CARACTERES AGE CONTENANT MELKI
+nom="Melki";
+alert("nom");
